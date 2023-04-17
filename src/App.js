6d1f37ef280d3f1692654253bfe2464c04d08a5a
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-17 12:10:58 +0300

    added button

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <h1>first react app</h1>
        <img src="https://jayswines.com/wp-content/uploads/2017/01/Buy-Hennessy-XO-700ml-online-in-Nairobi.png"alt="" srcset=""></img>
+       <div>
+        <button type='button'>order drinks</button>
+       </div>
       </header>
     </div>
   );
